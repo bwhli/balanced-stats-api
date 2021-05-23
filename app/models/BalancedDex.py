67198e8d862ed03dff9052104d62e35eb68c3b2c
Brian Li <brian@brianli.com>
2021-05-23 20:48:36 +0900

    Add BALN/sICX liquidity pool.

--- app/models/BalancedDex.py
+++ app/models/BalancedDex.py
@@ -13,7 +13,8 @@ class BalancedDex:
         sicx_icx_stats = self._icx.call(self._BALANCED_DEX_ADDRESS, "getPoolStats", {"_id": 1})  # noqa 503
         sicx_bnusd_stats = self._icx.call(self._BALANCED_DEX_ADDRESS, "getPoolStats", {"_id": 2})  # noqa 503
         baln_bnusd_stats = self._icx.call(self._BALANCED_DEX_ADDRESS, "getPoolStats", {"_id": 3})  # noqa 503
-        for pool in [sicx_icx_stats, sicx_bnusd_stats, baln_bnusd_stats]:
+        baln_sicx_stats = self._icx.call(self._BALANCED_DEX_ADDRESS, "getPoolStats", {"_id": 4})  # noqa 503
+        for pool in [sicx_icx_stats, sicx_bnusd_stats, baln_bnusd_stats, baln_sicx_stats]:  # noqa 503
             pool["base"] = str(hex_to_int(pool["base"], 18))
             pool["quote"] = str(hex_to_int(pool["quote"], 18))
             pool["price"] = str(hex_to_int(pool["price"], 18))
@@ -24,19 +25,22 @@ class BalancedDex:
         return {
             "sicx_icx_pool": sicx_icx_stats,
             "sicx_bnusd_pool": sicx_bnusd_stats,
-            "baln_bnusd_pool": baln_bnusd_stats
+            "baln_bnusd_pool": baln_bnusd_stats,
+            "baln_sicx_pool": baln_sicx_stats
         }
 
     def get_dex_tvl(self):
         sicx_icx_tvl = hex_to_int(self._icx.call(self._BALANCED_DEX_ADDRESS, "totalSupply", {"_id": 1}), 18)  # noqa 503
         sicx_bnusd_tvl = hex_to_int(self._icx.call(self._BALANCED_DEX_ADDRESS, "getPoolTotal", {"_id": 2, "_token": "cx88fd7df7ddff82f7cc735c871dc519838cb235bb"}), 18)  # noqa 503
         baln_bnusd_tvl = hex_to_int(self._icx.call(self._BALANCED_DEX_ADDRESS, "getPoolTotal", {"_id": 3, "_token": "cx88fd7df7ddff82f7cc735c871dc519838cb235bb"}), 18)  # noqa 503
+        sicx_baln_tvl = hex_to_int(self._icx.call(self._BALANCED_DEX_ADDRESS, "getPoolTotal", {"_id": 4, "_token": "cx88fd7df7ddff82f7cc735c871dc519838cb235bb"}), 18)  # noqa 503
         sicx_icx_tvl_usd = sicx_icx_tvl * self._icx_price
-        dex_total_tvl_usd = sicx_bnusd_tvl + baln_bnusd_tvl + sicx_icx_tvl_usd
+        dex_total_tvl_usd = sicx_bnusd_tvl + baln_bnusd_tvl + sicx_icx_tvl_usd + sicx_baln_tvl  # noqa 503
         return {
             "sicx_icx_tvl": round(sicx_icx_tvl, 8),
             "sicx_bnusd_tvl": round(sicx_bnusd_tvl, 2),
             "baln_bnusd_tvl": round(baln_bnusd_tvl, 2),
+            "sicx_baln_tvl": round(sicx_baln_tvl, 8),
             "dex_total_tvl_usd": round(dex_total_tvl_usd, 2)
         }
 
@@ -44,4 +48,5 @@ class BalancedDex:
         sicx_icx_quote = hex_to_int(self._icx.call(self._BALANCED_DEX_ADDRESS, "getPrice", {"_id": 1}), 18)  # noqa 503
         sicx_bnusd_quote = hex_to_int(self._icx.call(self._BALANCED_DEX_ADDRESS, "getPrice", {"_id": 2}), 18)  # noqa 503
         baln_bnusd_quote = hex_to_int(self._icx.call(self._BALANCED_DEX_ADDRESS, "getPrice", {"_id": 3}), 18)  # noqa 503
-        return sicx_icx_quote, sicx_bnusd_quote, baln_bnusd_quote
+        baln_sicx_quote = hex_to_int(self._icx.call(self._BALANCED_DEX_ADDRESS, "getQuotePriceInBase", {"_id": 4}), 18)  # noqa 503
+        return sicx_icx_quote, sicx_bnusd_quote, baln_bnusd_quote, baln_sicx_quote  # noqa 503
